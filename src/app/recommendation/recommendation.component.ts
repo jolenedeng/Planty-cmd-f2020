420afa9749fe3b28f049d9cbc0415ef9678299b4
Emily Gong <z.gong@alumni.ubc.ca>
2020-03-07 21:17:04 -0800

    search for current location

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import {} from "googlemaps";
 import {LocationService} from './location.service';
+import {StatesLibrary} from './statesLibrary';
 
 
 @Component({
@@ -12,8 +13,13 @@ import {LocationService} from './location.service';
  * Container for recommendation page.
  */
 export class RecommendationComponent {
+  private states: Map<string, string>;
 
-  constructor(private locationService: LocationService) {}
+  private currentState: string;
+
+  constructor(private locationService: LocationService, stateLibrary: StatesLibrary) {
+    this.states = stateLibrary.getStates();
+  }
 
   public findUser(): void {
     navigator.geolocation?.getCurrentPosition(((position: Position) => {
@@ -21,8 +27,16 @@ export class RecommendationComponent {
     }));
   }
   public showCurrentLocation(position: Position): void {
-    console.log("my location is " + position.coords.latitude + " " + position.coords.longitude);
-    this.locationService.getStateFromLatlng(position.coords.latitude, position.coords.longitude);
+    this.locationService.getStateFromLatlng(position.coords.latitude, position.coords.longitude).subscribe((res: any) => {
+      if (res.status === "OK" && res.results.length > 0) {
+        const found = res.results[0].address_components?.find((address) => {
+          Array.from(this.states.values()).includes(address.long_name);
+        });
+        if (found) {
+          this.currentState = found.long_name;
+        }
+      }
+    });
   }
 
 }
