ca5147268a70d13a9183a76cbfc9f14c04f29372
Jolene Deng <jolenedeng@dhcp-128-189-252-255.ubcsecure.wireless.ubc.ca>
2020-03-07 16:40:58 -0800

    Routing & navbar  boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { ClarityModule } from "@clr/angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { GardenComponent } from './garden/garden.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
     BrowserAnimationsModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FavoritesComponent,
+    GardenComponent,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
