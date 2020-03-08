c32cb875dcf62e94347c373cd354610d550817fb
Jolene Deng <jolenedeng@alumni.ubc.ca>
2020-03-07 16:40:58 -0800

    Routing & navbar  boilerplate code

--- src/app/appComponent.spec.ts
+++ src/app/appComponent.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  
+
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
---
+++ src/app/recommendation/recommendation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'recommendation',
+    templateUrl: './recommendation.component.ng.html'
+})
+/**
+ * Container for recommendation page.
+ */
+export class RecommendationComponent {
+
+}
