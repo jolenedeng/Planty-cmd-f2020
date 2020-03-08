f45b94b1104cb1940b13fb4b0940c2585b7f81cc
Emily Gong <z.gong@alumni.ubc.ca>
2020-03-07 21:32:05 -0800

    search for current location

--- src/app/recommendation/recommendation.component.ts
+++ src/app/recommendation/recommendation.component.ts
@@ -13,12 +13,14 @@ import {StatesLibrary} from './statesLibrary';
  * Container for recommendation page.
  */
 export class RecommendationComponent {
-  private states: Map<string, string>;
 
   private currentState: string;
 
-  constructor(private locationService: LocationService, stateLibrary: StatesLibrary) {
-    this.states = stateLibrary.getStates();
+  constructor(private locationService: LocationService) {
+  }
+
+  public get getCurrentState(): string {
+    return (this.currentState) ? this.currentState: "unknown location";
   }
 
   public findUser(): void {
@@ -30,10 +32,10 @@ export class RecommendationComponent {
     this.locationService.getStateFromLatlng(position.coords.latitude, position.coords.longitude).subscribe((res: any) => {
       if (res.status === "OK" && res.results.length > 0) {
         const found = res.results[0].address_components?.find((address) => {
-          Array.from(this.states.values()).includes(address.long_name);
+          return Array.from(StatesLibrary.STATES.values()).indexOf(address.long_name) !== -1;
         });
         if (found) {
-          this.currentState = found.long_name;
+          this.currentState = found.short_name;
         }
       }
     });
