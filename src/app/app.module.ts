55a86e46bdeb86ae51ac2dc927459f282bff43b3
tiffanyou <44226331+tiffanyou@users.noreply.github.com>
2020-03-07 18:56:23 -0800

    Create a plant dto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ClarityModule } from "@clr/angular";
+import { ClarityModule } from '@clr/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
