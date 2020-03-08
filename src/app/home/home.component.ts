ca72cc9ba13364a2b4b3dab9ff93e0f43ce38c49
Jolene Deng <jolenedeng@dhcp-128-189-252-255.ubcsecure.wireless.ubc.ca>
2020-03-07 17:28:18 -0800

    Add skeleton for home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.ng.html'
+    templateUrl: './home.component.ng.html',
+    styleUrls: ['./home.component.css']
 })
 /**
  * Main landing page of the app. Includes navigation to recommendations and plant identification.
